f1ce8236e927787f367097ce95a45e9e14458402
teademia <2408387759@qq.com>
2024-05-04 21:34:10 +0800

    修改超时时间为1000print正常输出

---
+++ build/CMakeFiles/my_tcp.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_tcp.dir/src/arp.c.o"
+  "CMakeFiles/my_tcp.dir/src/arp.c.o.d"
+  "CMakeFiles/my_tcp.dir/src/driver.c.o"
+  "CMakeFiles/my_tcp.dir/src/driver.c.o.d"
+  "CMakeFiles/my_tcp.dir/src/main.c.o"
+  "CMakeFiles/my_tcp.dir/src/main.c.o.d"
+  "my_tcp"
+  "my_tcp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/my_tcp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/my_tcp.dir/src/arp.c.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/my_tcp.dir/src/arp.c.o: /home/hp/project/my_tcp/src/arp.c \
+ /usr/include/stdc-predef.h
